14a9bfb476c04cd8d9a959c11e2e88fa0504e7ac
stefaxel <62843391+stefaxel@users.noreply.github.com>
2022-10-27 16:38:00 +0100

    Third Person Camera
    Third person camera added using CineMachine

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -7,11 +7,15 @@ public class PlayerController : MonoBehaviour
     [Header("Player Controls")]
     [SerializeField] float speed;
     [SerializeField] float jumpForce;
+    [SerializeField] float turnSmoothing;
+
     private bool isJumping;
     private float horizontalInput;
     private float verticalInput;
+    private float turnSmoothVelocity;
 
     private Rigidbody playerRb;
+    [SerializeField] Transform cam;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,8 +33,17 @@ public class PlayerController : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
         horizontalInput = Input.GetAxis("Horizontal");
 
-        Vector3 movement = new Vector3(horizontalInput, 0.0f, verticalInput);
+        Vector3 movement = new Vector3(horizontalInput, 0.0f, verticalInput).normalized;
 
-        playerRb.AddForce(movement * speed * Time.deltaTime);
+        if(movement.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothing);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            playerRb.AddForce(moveDirection.normalized * speed * Time.deltaTime);
+        }
+
     }
 }
