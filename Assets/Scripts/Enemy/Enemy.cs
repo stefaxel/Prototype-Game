4ce86458f728f3256a58a177e80fd2e0002c7948
stefaxel <62843391+stefaxel@users.noreply.github.com>
2022-11-03 17:01:33 +0000

    Added patrol for enemy
    Issue with the enemy not selecting a new patrol point once it has got to the destination

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -5,41 +5,99 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] protected float speed;
+    [Header("Enemy References")]
     protected static GameObject player;
     protected static Rigidbody enemyRb;
     [SerializeField] protected NavMeshAgent enemy;
     NavMeshHit hit;
 
+    [Header("Enemy Parameters")]
+    [SerializeField] protected float speed;
+    [SerializeField] protected Vector3 roamPoint;
+    [SerializeField] protected bool walkPointSet;
+    [SerializeField] protected float roamPointRangeX;
+    [SerializeField] protected float roamPointRangeZ;
+    [SerializeField] protected float negRoamPointRangeX;
+    [SerializeField] protected float negRoamPointRangeZ;
+    [SerializeField] protected LayerMask whatIsGround;
+    [SerializeField] protected LayerMask whatIsPlayer;
+
+    [Header("Enemy Attack")]
+    [SerializeField] protected float timeBetweenAttack;
+    protected bool alreadyAttacked;
+
+    [Header("Enemy States")]
+    [SerializeField] protected float sightRange;
+    [SerializeField] protected float attackRange;
+    protected bool playerInSight;
+    protected bool attackPlayer;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     { 
         player = GameObject.Find("Player");
         enemyRb = GetComponent<Rigidbody>();
+        enemy = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        playerInSight = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        attackPlayer = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
 
-        //Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+        if(!playerInSight && !attackPlayer)
+        {
+            Patrolling();
+        }
 
-        //enemyRb.AddForce(lookDirection * speed * Time.deltaTime);
-
-        //if (transform.position.y < -10)
-        //{
-        //    gameObject.SetActive(false);
-        //}
-
-        
-        
-        enemy.SetDestination(player.transform.position);
+        if(playerInSight && !attackPlayer)
+        {
+            ChasePlayer();
+        }
         
         if(transform.position.y < -10)
         {
             gameObject.SetActive(false);
         }
 
+    }
 
+    private void Patrolling()
+    {
+        if (!walkPointSet)
+        {
+            SearchPatrolPoint();
+        }
+        if (walkPointSet)
+        {
+            enemy.SetDestination(roamPoint);
+        }
+
+        Vector3 distanceToWalkPoint = transform.position - roamPoint;
+
+        if(distanceToWalkPoint.magnitude < 1f)
+        {
+            Debug.Log("Setting a new walk point");
+            walkPointSet = false;
+        }
+    }
+
+    private void SearchPatrolPoint()
+    {
+        float randomX = Random.Range(-negRoamPointRangeX, roamPointRangeX);
+        float randomZ = Random.Range(-negRoamPointRangeZ, roamPointRangeZ);
+
+        roamPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if(Physics.Raycast(roamPoint, -transform.up, 2f, whatIsGround))
+        {
+            walkPointSet = true;
+        }
+    }
+
+    private void ChasePlayer()
+    {
+        enemy.SetDestination(player.transform.position);
     }
 }
